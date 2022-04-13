8ce9f0782811bb9a2c4c3d3d95ce76a84fb760c4
evin16 <58085767+evin16@users.noreply.github.com>
2022-04-13 17:21:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Kelas from "./components/Kelas";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Navbar />
+        {/* <Home /> */}
+        <Register />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Kelas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Kelas() {
+  return (
+    <div>Kelas</div>
+  )
+}
