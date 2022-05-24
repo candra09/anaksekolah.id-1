5f6e954632ab8df314c7dd94c103218eb27912cf
candra09 <72352882+candra09@users.noreply.github.com>
2022-05-24 14:49:12 +0700

    atur pwa 5/24/2022

---
+++ src/components/Offline/index.js
@@ -0,0 +1,9 @@
+function Offline(){
+    return(
+<div class="alert alert-danger" role="alert">
+  <strong>Kamu sedang offline!</strong>  Jangan khawatir, kamu masih bisa melakukan banyak hal.
+</div>
+    )
+}
+export default Offline;
+
